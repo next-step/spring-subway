5233ec2262b55f327ae47bf1e067356486a836b9
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-16 23:49:41 +0900

    feat: 그래프 구간 추가

--- src/main/java/subway/domain/SubwayGraph.java
+++ src/main/java/subway/domain/SubwayGraph.java
@@ -1,21 +1,87 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import lombok.AccessLevel;
+import lombok.Getter;
 
+import java.util.*;
+
+@Getter(AccessLevel.PACKAGE)
 public class SubwayGraph {
-    private Map<Station, List<Section>> connection = new HashMap<>();
+    /**
+     * 해당 역과 연결되어 있는 구간들을 저장합니다.
+     */
+    private Map<Station, Set<Section>> connection = new HashMap<>();
+
+    /**
+     * 각 호선의 첫번째 역을 저장합니다.
+     * 노선별 탐색에 사용됩니다.
+     */
+    private Map<Line, Station> firstStationInLines = new HashMap<>();
 
     public SubwayGraph() {}
 
+    /**
+     * 지하철 노선 그래프에 구간을 추가합니다.
+     * 1. 새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 합니다.
+     * 2. 노선에 이미 등록되어있는 역은 새로운 구간의 하행역이 될 수 없습니다.
+     * @param section
+     */
     public void add(Section section) {
-        connection.computeIfAbsent(section.getStation(), (unused) -> new ArrayList<>()).add(section);
+        if (!isLastDownStationInLine(section)) {
+            throw new IllegalStateException("기존의 하행 종점역에만 추가할 수 있습니다.");
+        }
+
+        if (isAlreadyExistsInLine(section.getLine(), section.getDownStation())) {
+            throw new IllegalStateException("이미 해당 노선에 존재하는 역은 추가할 수 없습니다.");
+        }
+        connection.computeIfAbsent(section.getUpStation(), (unused) -> new HashSet<>()).add(section);
+        firstStationInLines.putIfAbsent(section.getLine(), section.getUpStation());
     }
 
-    public List<Section> getSections(Station station) {
-        System.out.println(connection);
+    private boolean isAlreadyExistsInLine(Line line, Station newStation) {
+        if(!firstStationInLines.containsKey(line)) {
+            return false;
+        }
+        Station stationInLine = firstStationInLines.get(line);
+        if (stationInLine.equals(newStation)) {
+            return true;
+        }
+        while (true) {
+            if (!connection.containsKey(stationInLine)) {
+                return false;
+            }
+            Optional<Section> nextSection = connection.get(stationInLine).stream()
+                    .filter(section -> section.getLine().equals(line))
+                    .findAny();
+            if (nextSection.isEmpty()) {
+                return false;
+            }
+            Station nextStation = nextSection.get().getDownStation();
+            if (nextStation.equals(newStation)) {
+                return true;
+            }
+            stationInLine = nextStation;
+        }
+    }
+
+    /**
+     * 추가할 구간의 상행역이 해당 라인의 하행 종점역인지 반환합니다.
+     * @param section
+     * @return
+     */
+    private boolean isLastDownStationInLine(Section section) {
+        if (!connection.containsKey(section.getUpStation())) {
+            return true;
+        }
+        Set<Section> sections = connection.get(section.getUpStation());
+        Optional<Section> connectedSectionInSameLine = sections.stream()
+                .filter(connectSection
+                        -> connectSection.getLine().equals(section.getLine())).findAny();
+        return connectedSectionInSameLine.isEmpty();
+    }
+
+    public Set<Section> getSections(Station station) {
         return connection.get(station);
     }
+
 }
--- src/test/java/subway/domain/SubwayGraphTest.java
+++ src/test/java/subway/domain/SubwayGraphTest.java
@@ -1,6 +1,5 @@
 package subway.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +9,8 @@ import static org.assertj.core.api.Assertions.*;
 class SubwayGraphTest {
 
     @Test
-    @DisplayName("그래프에 구간을 하나 추가한다.")
-    void addSectionToGraph(){
+    @DisplayName("구간 추가에 성공합니다")
+    void addSectionSuccessfully() {
         // given
         Line line = new Line(1L, "1");
         Station upStation = new Station(1L);
@@ -26,6 +25,71 @@ class SubwayGraphTest {
 
         // then
         assertThat(subwayGraph.getSections(upStation)).hasSize(1);
-        assertThat(subwayGraph.getSections(upStation).get(0)).isEqualTo(section);
+        assertThat(subwayGraph.getSections(upStation)).contains(section);
+    }
+
+    @Test
+    @DisplayName("추가하려는 구간의 역이 하행종점역이 아니라서 실패합니다.")
+    void addFailBecauseOfNotLastStation() {
+        // given
+        SubwayGraph subwayGraph = new SubwayGraph();
+        subwayGraph.add(makeSection(1L, 1L, 2L, 1));
+        subwayGraph.add(makeSection(1L, 2L, 3L, 1));
+        subwayGraph.add(makeSection(1L, 3L, 4L, 1));
+
+        Section addSection = makeSection(1L, 2L, 5L, 1);
+
+        // when
+        assertThatThrownBy(() -> {
+            subwayGraph.add(addSection);
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessage("기존의 하행 종점역에만 추가할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("추가하려는 역(구간의 하행역)이 노선에 이미 있어서 실패합니다.")
+    void addFailBecauseOfAlreadyExists() {
+        // given
+        SubwayGraph subwayGraph = new SubwayGraph();
+        subwayGraph.add(makeSection(1L, 1L, 2L, 1));
+        subwayGraph.add(makeSection(1L, 2L, 3L, 1));
+        subwayGraph.add(makeSection(1L, 3L, 4L, 1));
+
+        System.out.println(subwayGraph.getFirstStationInLines());
+
+        Section addSection = makeSection(1L, 4L, 1L, 1);
+
+        // when
+        assertThatThrownBy(() -> {
+            subwayGraph.add(addSection);
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 해당 노선에 존재하는 역은 추가할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("노선의 하행 종점역에 구간 추가를 성공합니다.")
+    void addSectionToLastDownStationSuccessfully() {
+        // given
+        SubwayGraph subwayGraph = new SubwayGraph();
+        subwayGraph.add(makeSection(1L, 1L, 2L, 1));
+        subwayGraph.add(makeSection(1L, 2L, 3L, 1));
+        subwayGraph.add(makeSection(1L, 3L, 4L, 1));
+
+        Section addSection = makeSection(1L, 4L, 5L, 1);
+
+        // when
+        subwayGraph.add(addSection);
+
+        // then
+        assertThat(subwayGraph.getSections(addSection.getUpStation())).hasSize(1);
+        assertThat(subwayGraph.getSections(addSection.getUpStation())).contains(addSection);
+    }
+
+    private static Section makeSection(long lineId, long stationId, long downStationId, Integer distance) {
+        Line line = new Line(lineId, "name");
+        Station upStation = new Station(stationId);
+        Station downStation = new Station(downStationId);
+
+        return Section.of(line, upStation, downStation, distance);
     }
 }
