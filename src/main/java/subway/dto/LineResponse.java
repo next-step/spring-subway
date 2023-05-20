61587a55cc1a48723964800f98cafc05fb9c1ae5
road-jin <rjswmtk@gmail.com>
2023-05-20 12:18:20 +0900

    refactor: LIneResponse에서 Station 도메인을 반환하지 않고 StationResponse를 반환하게 변경

--- src/main/java/subway/dto/LineResponse.java
+++ src/main/java/subway/dto/LineResponse.java
@@ -1,24 +1,26 @@
 package subway.dto;
 
+import java.util.Collections;
 import java.util.List;
 import subway.domain.Line;
 import subway.domain.Station;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private List<Station> stations;
+
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final List<StationResponse> stations;
 
     public LineResponse(Long id, String name, String color, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.stations = StationResponse.from(stations);
     }
 
     public static LineResponse from(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), null);
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), Collections.emptyList());
     }
 
     public static LineResponse of(Line line, List<Station> stations) {
@@ -37,7 +39,7 @@ public class LineResponse {
         return color;
     }
 
-    public List<Station> getStations() {
+    public List<StationResponse> getStations() {
         return stations;
     }
 }
