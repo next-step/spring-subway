ebc0632619893dcc6d14f5b0c945b00dbc9701d1
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-21 19:14:47 +0900

    refactor(dao): SectionRowMapper 클래스 추가

--- src/main/java/subway/persistence/jdbcDao/SectionDao.java
+++ src/main/java/subway/persistence/jdbcDao/SectionDao.java
@@ -1,16 +1,10 @@
 package subway.persistence.jdbcDao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import subway.domain.Line;
 import subway.domain.Section;
-import subway.domain.Station;
 import subway.domain.repository.SectionRepository;
-import subway.domain.vo.Distance;
-
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
@@ -19,45 +13,17 @@ import java.util.Map;
 @Repository
 public class SectionDao implements SectionRepository {
 
-    private JdbcTemplate jdbcTemplate;
-    private SimpleJdbcInsert insertAction;
-    private StationDao stationDao;
-    private LineDao lineDao;
-
-    private RowMapper<Section> rowMapper = (rs, rowNum) -> {
-        long lineId = rs.getLong("line_id");
-        long upStationId = rs.getLong("up_station_id");
-        long downStationId = rs.getLong("down_station_id");
-
-        Line line = lineDao.findById(lineId);
-        Station upStation = stationDao.findById(upStationId);
-        Station downStation = stationDao.findById(downStationId);
-
-        return Section.builder()
-                .id(rs.getLong("id"))
-                .line(line)
-                .upStation(upStation)
-                .downStation(downStation)
-                .distance(new Distance(rs.getInt("distance")))
-                .build();
-    };
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertAction;
+    private final SectionRowMapper rowMapper;
 
     public SectionDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.insertAction = new SimpleJdbcInsert(dataSource)
                 .withTableName("SECTION")
                 .usingGeneratedKeyColumns("id");
-        this.stationDao = new StationDao(dataSource);
-        this.lineDao = new LineDao(dataSource);
+        rowMapper = new SectionRowMapper(new StationDao(dataSource), new LineDao(dataSource));
     }
-
-//    @Autowired
-//    public void setDataSource(DataSource dataSource) {
-//        this.jdbcTemplate = new JdbcTemplate(dataSource);
-//        this.insertAction = new SimpleJdbcInsert(dataSource)
-//                .withTableName("SECTION")
-//                .usingGeneratedKeyColumns("id");
-//    }
 
     @Override
     public Section insert(Section section) {
