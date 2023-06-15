5c2ebefb3d0be5344d6bcd3beb92f4d08c3be53e
jisu <jisuuoo96@gmail.com>
2023-06-15 14:47:40 +0900

    test: 구간 삭제 기능 테스트 작성

--- src/test/java/subway/unit/SectionServiceTest.java
+++ src/test/java/subway/unit/SectionServiceTest.java
@@ -10,7 +10,11 @@ import subway.application.LineService;
 import subway.application.SectionService;
 import subway.application.StationService;
 import subway.dao.SectionDao;
+import subway.domain.Section;
 import subway.dto.*;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -45,19 +49,19 @@ class SectionServiceTest {
     void saveSectionTest() {
 
         // given
-        var 신사역 = stationService.saveStation(new StationRequest("신사역"));
-        var 강남역 = stationService.saveStation(new StationRequest("강남역"));
-        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red"));
+        var 신사역 = stationService.saveStation(new StationRequest("신사역")).getId();
+        var 강남역 = stationService.saveStation(new StationRequest("강남역")).getId();
+        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red")).getId();
 
         // when
-        var response = sectionService.save(신분당선.getId(), new SectionRequest(신사역.getId(), 강남역.getId(), 12));
+        var response = sectionService.save(신분당선, new SectionRequest(신사역, 강남역, 12));
 
         // then
 
         assertAll(
                 () -> assertThat(response.getId()).isNotNull(),
-                () -> assertThat(response.getUpStationId()).isEqualTo(신사역.getId()),
-                () -> assertThat(response.getDownStationId()).isEqualTo(강남역.getId())
+                () -> assertThat(response.getUpStationId()).isEqualTo(신사역),
+                () -> assertThat(response.getDownStationId()).isEqualTo(강남역)
         );
     }
 
@@ -66,19 +70,19 @@ class SectionServiceTest {
     void saveSectionExceptionTest() {
 
         // given
-        var 신사역 = stationService.saveStation(new StationRequest("신사역"));
-        var 강남역 = stationService.saveStation(new StationRequest("강남역"));
-        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red"));
-        sectionService.save(신분당선.getId(), new SectionRequest(신사역.getId(), 강남역.getId(), 12));
+        var 신사역 = stationService.saveStation(new StationRequest("신사역")).getId();
+        var 강남역 = stationService.saveStation(new StationRequest("강남역")).getId();
+        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red")).getId();
+        sectionService.save(신분당선, new SectionRequest(신사역, 강남역, 12));
 
-        var 양재역 = stationService.saveStation(new StationRequest("양재역"));
-        var 미금역 = stationService.saveStation(new StationRequest("미금역"));
+        var 양재역 = stationService.saveStation(new StationRequest("양재역")).getId();
+        var 미금역 = stationService.saveStation(new StationRequest("미금역")).getId();
 
 
-        var 추가하려는_구간 = new SectionRequest(양재역.getId(), 미금역.getId(), 5);
+        var 추가하려는_구간 = new SectionRequest(양재역, 미금역, 5);
 
         // when & then
-        assertThatThrownBy(() -> sectionService.save(신분당선.getId(), 추가하려는_구간))
+        assertThatThrownBy(() -> sectionService.save(신분당선, 추가하려는_구간))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -87,18 +91,51 @@ class SectionServiceTest {
     void saveSectionExceptionTest1() {
 
         // given
-        var 신사역 = stationService.saveStation(new StationRequest("신사역"));
-        var 강남역 = stationService.saveStation(new StationRequest("강남역"));
-        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red"));
-        sectionService.save(신분당선.getId(), new SectionRequest(신사역.getId(), 강남역.getId(), 12));
+        var 신사역 = stationService.saveStation(new StationRequest("신사역")).getId();
+        var 강남역 = stationService.saveStation(new StationRequest("강남역")).getId();
+        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red")).getId();
+        sectionService.save(신분당선, new SectionRequest(신사역, 강남역, 12));
 
-        var 추가하려는_구간 = new SectionRequest(강남역.getId(), 신사역.getId(), 3);
+        var 추가하려는_구간 = new SectionRequest(강남역, 신사역, 3);
 
         // when & then
-        assertThatThrownBy(() -> sectionService.save(신분당선.getId(), 추가하려는_구간))
+        assertThatThrownBy(() -> sectionService.save(신분당선, 추가하려는_구간))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+
+    @Test
+    @DisplayName("구간 삭제 테스트")
+    void deleteSectionTest() {
+
+        // given
+        var 신사역 = stationService.saveStation(new StationRequest("신사역")).getId();
+        var 강남역 = stationService.saveStation(new StationRequest("강남역")).getId();
+        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red")).getId();
+        sectionService.save(신분당선, new SectionRequest(신사역, 강남역, 3));
+
+        // when
+        sectionService.delete(신분당선, 강남역);
+
+        // then
+        List<Section> sections = sectionDao.findAllByLineId(신분당선);
+
+        assertThat(sections).isEmpty();
+    }
+
+    @Test
+    @DisplayName("구간 삭제 예외 테스트 - 삭제하려는 구간이 마지막 구간이 아닐 경우")
+    void deleteSectionExceptionTest() {
+
+        // given
+        var 신사역 = stationService.saveStation(new StationRequest("신사역")).getId();
+        var 강남역 = stationService.saveStation(new StationRequest("강남역")).getId();
+        var 신분당선 = lineService.saveLine(new LineRequest("신분당선", "red")).getId();
+        sectionService.save(신분당선, new SectionRequest(신사역, 강남역, 3));
+
+        // when & then
+        assertThatThrownBy(() -> sectionService.delete(신분당선, 신사역)).isInstanceOf(IllegalArgumentException.class);
+    }
 
 
 }
