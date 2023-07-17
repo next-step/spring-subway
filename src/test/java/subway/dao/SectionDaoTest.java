205e2ca37d8bc888809eeb0cac74147c4cc284d5
김명준 <audwnswmf@naver.com>
2023-07-17 16:34:36 +0900

    refactor: sectionDao.insert 리팩토링

--- src/test/java/subway/dao/SectionDaoTest.java
+++ src/test/java/subway/dao/SectionDaoTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import subway.domain.Line;
 import subway.domain.Section;
-import subway.domain.Station;
 
 
 @SpringBootTest
@@ -16,12 +14,6 @@ class SectionDaoTest {
 
     @Autowired
     private SectionDao sectionDao;
-
-    @Autowired
-    private StationDao stationDao;
-
-    @Autowired
-    private LineDao lineDao;
 
     @DisplayName("노선아이디와 상행역아이디와 하행역아이디와 거리를 가지고 구간을 생성한다.")
     @Test
@@ -32,9 +24,6 @@ class SectionDaoTest {
         Long downStationId = 2L;
         Long distance = 10L;
         Section section = new Section(lineId, upStationId, downStationId, distance);
-        stationDao.insert(new Station("a"));
-        stationDao.insert(new Station("b"));
-        lineDao.insert(new Line("line1", "blue"));
 
         // when
         Section result = sectionDao.insert(section);
