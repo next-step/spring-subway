d1143fb1d10acb22a24e1c264893243ceb174e44
김명준 <audwnswmf@naver.com>
2023-07-17 16:20:09 +0900

    feat: SectionDao.insert 구현

--- src/main/java/subway/dao/SectionDao.java
+++ src/main/java/subway/dao/SectionDao.java
@@ -1,13 +1,44 @@
 package subway.dao;
 
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import subway.domain.Section;
 
 @Repository
 public class SectionDao {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertAction;
 
-    public Section save(Section section) {
-        return null;
+    private RowMapper<Section> rowMapper = (rs, rowNum) ->
+            new Section(
+                    rs.getLong("id"),
+                    rs.getLong("line_id"),
+                    rs.getLong("up_station_id"),
+                    rs.getLong("down_station_id"),
+                    rs.getLong("distance")
+            );
+
+    public SectionDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.insertAction = new SimpleJdbcInsert(dataSource)
+                .withTableName("section")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public Section insert(Section section) {
+        SqlParameterSource params = new BeanPropertySqlParameterSource(section);
+        Long id = insertAction.executeAndReturnKey(params).longValue();
+        return new Section(
+                id,
+                section.getLineId(),
+                section.getUpStationId(),
+                section.getDownStationId(),
+                section.getDistance());
     }
 }
--- src/test/java/subway/dao/SectionDaoTest.java
+++ src/test/java/subway/dao/SectionDaoTest.java
@@ -4,25 +4,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import subway.domain.Line;
 import subway.domain.Section;
+import subway.domain.Station;
 
+
+@SpringBootTest
 class SectionDaoTest {
 
+    @Autowired
+    private SectionDao sectionDao;
+
+    @Autowired
+    private StationDao stationDao;
+
+    @Autowired
+    private LineDao lineDao;
 
     @DisplayName("노선아이디와 상행역아이디와 하행역아이디와 거리를 가지고 구간을 생성한다.")
     @Test
-    void save() {
+    void insert_success() {
         // given
         Long lineId = 1L;
-        Long upStationId = 2L;
-        Long downStationId = 4L;
+        Long upStationId = 1L;
+        Long downStationId = 2L;
         Long distance = 10L;
         Section section = new Section(lineId, upStationId, downStationId, distance);
-
-        SectionDao sectionDao = new SectionDao();
+        stationDao.insert(new Station("a"));
+        stationDao.insert(new Station("b"));
+        lineDao.insert(new Line("line1", "blue"));
 
         // when
-        Section result = sectionDao.save(section);
+        Section result = sectionDao.insert(section);
 
         // then
         assertThat(result.getId()).isNotNull();
