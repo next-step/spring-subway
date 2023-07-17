762639a7c33967aa1c3318646d728e612ffa0721
chanwooDev <pove2019@gmail.com>
2023-07-17 16:19:18 +0900

    feat: Section / 상행역과 하행역은 같을 수 없다

--- src/test/java/subway/domain/Section.java
+++ src/test/java/subway/domain/Section.java
@@ -1,11 +1,24 @@
 package subway.domain;
 
 public class Section {
+
+    private final Station upStation;
+    private final Station downStation;
     private final int distance;
 
-    public Section(int distance) {
+    public Section(Station upStation, Station downStation, int distance) {
         validatePositive(distance);
+
+        if (upStation.equals(downStation)) {
+            throw new IllegalArgumentException(
+                "구간의 상행역과 하행역은 같을 수 없습니다. upStation: " + upStation + ", downStation: "
+                    + downStation);
+        }
+
+        this.upStation = upStation;
+        this.downStation = downStation;
         this.distance = distance;
+
     }
 
     private void validatePositive(int distance) {
--- src/test/java/subway/domain/SectionTest.java
+++ src/test/java/subway/domain/SectionTest.java
@@ -7,12 +7,22 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Section 클래스 테스트")
-public class SectionTest {
+class SectionTest {
 
     @Test
     @DisplayName("구간 길이는 양수여야 한다.")
     void sectionDistanceShouldBePositive() {
-        assertThatCode(() -> new Section(1)).doesNotThrowAnyException();
-        assertThatThrownBy(() -> new Section(-1)).isInstanceOf(IllegalArgumentException.class);
+        Station upStation = new Station(1L, "stationA");
+        Station downStation = new Station(2L, "stationB");
+
+        assertThatCode(() -> new Section(upStation, downStation, 1)).doesNotThrowAnyException();
+        assertThatThrownBy(() -> new Section(upStation, downStation, -1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상행역과 하행역은 같을 수 없다")
+    void upStationAndDownStationShouldNotEqual() {
+        Station station = new Station(1L, "stationA");
+        assertThatThrownBy(() -> new Section(station, station, 1)).isInstanceOf(IllegalArgumentException.class);
     }
 }
