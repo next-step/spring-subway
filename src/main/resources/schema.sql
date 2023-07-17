466f9002bca311f6c2f53462bfc405c878b6c8c4
whatasame <whatasame@gmail.com>
2023-07-17 16:53:11 +0900

    docs: 구간 테이블 스키마 정의
    Co-authored-by: protoseo <proto_seo@naver.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,26 @@
-create table if not exists STATION
-(
-    id bigint auto_increment not null,
-    name varchar(255) not null unique,
-    primary key(id)
-);
-
 create table if not exists LINE
 (
     id bigint auto_increment not null,
     name varchar(255) not null unique,
     color varchar(20) not null,
+    primary key(id)
+);
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id bigint not null,
+    up_station_id bigint not null,
+    down_station_id bigint not null,
+    pre_section_id bigint,
+    post_section_id bigint,
+    primary key(id),
+    foreign key (line_id) references LINE(id)
+);
+
+create table if not exists STATION
+(
+    id bigint auto_increment not null,
+    name varchar(255) not null unique,
     primary key(id)
 );
