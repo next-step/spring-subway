8c2475d450e2efc7daaf7ea1a211840cbbd64942
devxb <dlwnsdud205@naver.com>
2023-07-17 15:02:20 +0900

    feat: Cors 설정을 추가한다

---
+++ src/main/java/subway/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package subway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000");
+    }
+}
