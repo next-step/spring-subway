5a7a741bbbe43a42a674465000aaf883f095f735
devxb <dlwnsdud205@naver.com>
2023-07-17 17:58:09 +0900

    feat: Section.connectDownSection은 middle station이 같으면 연결 된다

--- src/main/java/subway/domain/Section.java
+++ src/main/java/subway/domain/Section.java
@@ -1,10 +1,38 @@
 package subway.domain;
 
+import java.text.MessageFormat;
 import org.springframework.util.Assert;
 
 public class Section {
+
+    private static final Section NOT_CONNECTED_SECTION = new Section();
+
+    private final Station upStation;
+    private final Station downStation;
+    private Section upSection = NOT_CONNECTED_SECTION;
+    private Section downSection = NOT_CONNECTED_SECTION;
+
     Section(Station upStation, Station downStation) {
-        Assert.notNull(upStation, () -> "upStation은 null이 될 수 없습니다. upStation");
-        Assert.notNull(downStation, () -> "downStation은 null이 될 수 없습니다. downStation");
+        Assert.notNull(upStation, () -> "upStation은 null이 될 수 없습니다.");
+        Assert.notNull(downStation, () -> "downStation은 null이 될 수 없습니다.");
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    private Section() {
+        upStation = null;
+        downStation = null;
+    }
+
+    void connectDownSection(Section downSection) {
+        Assert.notNull(downSection, () -> "downSection은 null이 될 수 없습니다.");
+        Assert.isTrue(downSection.upStation == downStation, () -> MessageFormat.format(
+                "추가되는 downSection.upStation은 현재의 section.downStation과 동일해야합니다. downSection.upStation \"{0}\" current.downStation \"{1}\"",
+                downSection.upStation, downStation));
+        this.downSection = downSection;
+    }
+
+    public Section getDownsection() {
+        return downSection;
     }
 }
