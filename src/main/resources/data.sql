f2b866672bc27c5e39bdfd0067af69ba520b6a74
jjungk <jsk33a@gmail.com>
2023-07-17 17:45:43 +0900

    feat: sql schema 쿼리 추가 및 data 쿼리 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into line(name, color) values ('7호선', '주황');
+
+insert into station(name) values ('부천시청역');
+insert into station(name) values ('신중동역');
+
+insert into section(up_station_id, down_station_id, line_id, distance) values (1, 2, 1, 10);
