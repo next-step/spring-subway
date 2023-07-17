0ff476ae2ca87126d3474cd0927e3f0ae1874d45
devxb <dlwnsdud205@naver.com>
2023-07-17 20:23:54 +0900

    test: SectionService.saveByStationId는 null이 들어오면 예외를 던진다

--- src/test/java/subway/application/SectionServiceTest.java
+++ src/test/java/subway/application/SectionServiceTest.java
@@ -1,13 +1,13 @@
 package subway.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,8 +43,8 @@ class SectionServiceTest {
             Long downStationId = 2L;
             String downStationName = "downStation";
 
-            Mockito.when(stationDao.findById(upStationId)).thenReturn(new Station(upStationId, upStationName));
-            Mockito.when(stationDao.findById(downStationId)).thenReturn(new Station(downStationId, downStationName));
+            when(stationDao.findById(upStationId)).thenReturn(new Station(upStationId, upStationName));
+            when(stationDao.findById(downStationId)).thenReturn(new Station(downStationId, downStationName));
 
             // when
             Exception exception = Assertions.catchException(
@@ -52,6 +52,21 @@ class SectionServiceTest {
 
             // then
             assertThat(exception).isNull();
+        }
+
+        @Test
+        @DisplayName("Null stationId가 들어오면, IllegalArgumentException을 던진다.")
+        void Throw_IllegalArgumentException_When_stationId_Is_Null() {
+            // given
+            Long notNullStationId = 1L;
+            Long nullStationId = null;
+
+            // when
+            Exception exception = Assertions.catchException(
+                    () -> sectionService.saveByStationId(nullStationId, notNullStationId));
+
+            // then
+            assertThat(exception).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
