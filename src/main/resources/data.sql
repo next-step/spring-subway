0e6231112ad6b12c4218181593d54719dd8bfc7f
김명준 <audwnswmf@naver.com>
2023-07-17 16:34:02 +0900

    feat: LINE , STATION 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into LINE(id, name, color)
+values ('1', '1호선', 'blue');
+insert into LINE (id, name, color)
+values ('2', '2호선', 'green');
+
+insert into STATION(id, name)
+values ('1', '인천');
+insert into STATION(id, name)
+values ('2', '오이도');
+insert into STATION(id, name)
+values ('3', '잠실');
+insert into STATION(id, name)
+values ('4', '낙성대');
+insert into STATION(id, name)
+values ('5', '사당');
