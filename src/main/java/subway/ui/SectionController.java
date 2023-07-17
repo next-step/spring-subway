95abc06e9dafcbcd23edde955cd22797133abad6
김명준 <audwnswmf@naver.com>
2023-07-17 17:40:56 +0900

    feat : SectionController.createSection 테스트 및 구현

--- src/main/java/subway/ui/SectionController.java
+++ src/main/java/subway/ui/SectionController.java
@@ -1,8 +1,31 @@
 package subway.ui;
 
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import subway.application.SectionService;
+import subway.dto.SectionRequest;
+import subway.dto.SectionResponse;
 
 @RestController
 public class SectionController {
 
+    private final SectionService sectionService;
+
+    public SectionController(SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
+    @PostMapping("/lines/{lineId}/sections")
+    public ResponseEntity<SectionResponse> createSection(
+            @PathVariable Long lineId,
+            @RequestBody SectionRequest sectionRequest) {
+        SectionResponse sectionResponse = sectionService.saveSection(lineId, sectionRequest);
+        return ResponseEntity.created(
+                        URI.create("/lines/" + lineId + "/sections/" + sectionResponse.getId()))
+                .body(sectionResponse);
+    }
 }
---
+++ src/test/java/subway/integration/SectionIntegrationTest.java
@@ -0,0 +1,49 @@
+package subway.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import subway.dto.SectionRequest;
+
+@DisplayName("지하철 구간 관련 기능")
+public class SectionIntegrationTest extends IntegrationTest {
+
+    private Long lineId;
+    private SectionRequest sectionRequest1;
+    private SectionRequest sectionRequest2;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        lineId = 1L;
+        sectionRequest1 = new SectionRequest(1L, 2L, 10L);
+        sectionRequest2 = new SectionRequest(2L, 3L, 10L);
+    }
+
+    @DisplayName("지하철 구간을 생성한다.")
+    @Test
+    void createLine() {
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(sectionRequest1)
+                .when().post("/lines/{lineId}/sections", lineId)
+                .then().log().all().
+                extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+
+
+}
