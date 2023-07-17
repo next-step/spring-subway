5316641fcf8c64569710d9cc479bb1ec8ccbe9ce
dongjoo0-0 <kdj486347@gmail.com>
2023-07-17 16:12:23 +0900

    test: 비어있는 구간으로 생성하는 테스트 코드 작성

--- src/test/java/subway/domain/SectionsTest.java
+++ src/test/java/subway/domain/SectionsTest.java
@@ -3,6 +3,7 @@ package subway.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +15,12 @@ class SectionsTest {
     void insertNewSection() {
         Sections sections = new Sections(List.of(new Section()));
         assertDoesNotThrow(() -> sections.insert(new Section()));
+    }
+
+    @DisplayName("비어있는 구간으로 생성하는 테스트")
+    @Test
+    void createByEmptySection() {
+        assertThrows(IllegalArgumentException.class, () -> new Sections(Collections.emptyList()));
     }
 
     @DisplayName("새로운 구간의 상행역이 하행 종점역과 다를 경우 예외로 처리")
