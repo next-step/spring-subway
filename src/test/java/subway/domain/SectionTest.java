1b90d98c5979815f9e254ec6ba02aaa6a1a01fb6
devxb <dlwnsdud205@naver.com>
2023-07-17 17:58:00 +0900

    test: Section.connectDownSection은 middle station이 같으면 연결 된다

--- src/test/java/subway/domain/SectionTest.java
+++ src/test/java/subway/domain/SectionTest.java
@@ -1,5 +1,8 @@
 package subway.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -23,7 +26,7 @@ class SectionTest {
             Throwable throwable = Assertions.catchThrowable(() -> new Section(upStation, downStation));
 
             // then
-            Assertions.assertThat(throwable).isNull();
+            assertThat(throwable).isNull();
         }
 
         @Test
@@ -34,10 +37,70 @@ class SectionTest {
             Station nullStation = null;
 
             // when
-            Exception exception = Assertions.catchException(() -> new Section(upStation, nullStation));
+            Exception exception = catchException(() -> new Section(upStation, nullStation));
 
             // then
-            Assertions.assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+            assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("connectDownSection 메소드는")
+    class ConnectDownSection_Method {
+
+        @Test
+        @DisplayName("각 Section의 middle station이 동일하면, upStation이 Section에 상행에 연결된다.")
+        void Connect_Down_Section_When_Input_Section() {
+            // given
+            Station upStation = new Station(1L, "upStation");
+            Station middleStation = new Station(1L, "middleStation");
+            Station downStation = new Station(2L, "downStation");
+
+            Section section = new Section(upStation, middleStation);
+            Section downSection = new Section(middleStation, downStation);
+
+            // when
+            section.connectDownSection(downSection);
+            Section result = section.getDownsection();
+
+            // then
+            assertThat(result).isEqualTo(downSection);
+        }
+
+        @Test
+        @DisplayName("각 section의 middle station이 다르면, IllegalArgumentException을 던진다.")
+        void Throw_IllegalArgumentException_When_Input_Different_Middle_Station() {
+            // given
+            Station upStation = new Station(1L, "upStation");
+            Station middleStation = new Station(1L, "upStation");
+            Station differentMiddleStation = new Station(1L, "upStation");
+            Station downStation = new Station(2L, "downStation");
+
+            Section section = new Section(upStation, middleStation);
+            Section downSection = new Section(differentMiddleStation, downStation);
+
+            // when
+            Exception exception = catchException(() -> section.connectDownSection(downSection));
+
+            // then
+            assertThat(exception).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("section으로 null이 들어오면, IllegalArgumentException을 던진다.")
+        void Throw_IllegalArgumentException_When_Input_Null_Section() {
+            // given
+            Station upStation = new Station(2L, "upStation");
+            Station middleStation = new Station(1L, "middleStation");
+
+            Section section = new Section(upStation, middleStation);
+            Section downSection = null;
+
+            // when
+            Exception exception = catchException(() -> section.connectDownSection(downSection));
+
+            // then
+            assertThat(exception).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
