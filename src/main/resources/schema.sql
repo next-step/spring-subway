79c5df4590a0195a0b01b79fc487e03f6fcd1ef8
chanwooDev <pove2019@gmail.com>
2023-07-18 19:26:12 +0900

    fix: section schema 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,9 +16,9 @@ create table if not exists LINE
 create table if not exists SECTION
 (
     id bigint auto_increment not null,
-    line_id not null,
-    up_station_id not null,
-    down_station_id not null,
+    line_id bigint not null,
+    up_station_id bigint not null,
+    down_station_id bigint not null,
     distance int,
     primary key(id)
     );
