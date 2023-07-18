4c5d27fc01d16b74409b7b742fe3becf95eb9f3b
chanwooDev <pove2019@gmail.com>
2023-07-18 14:06:53 +0900

    feat: section table 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,13 @@ create table if not exists LINE
     color varchar(20) not null,
     primary key(id)
 );
+
+create table if not exists SECTION
+(
+    id bigint auto_increment not null,
+    line_id not null,
+    up_station_id not null,
+    down_station_id not null,
+    distance int,
+    primary key(id)
+    );
