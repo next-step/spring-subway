ef859cc92268f2f75416c80cb0efbffd4e00ad27
devxb <dlwnsdud205@naver.com>
2023-07-19 00:38:27 +0900

    feat: LineController.createSection API를 추가한다

--- src/main/java/subway/ui/LineController.java
+++ src/main/java/subway/ui/LineController.java
@@ -1,14 +1,22 @@
 package subway.ui;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import subway.application.LineService;
-import subway.dto.LineRequest;
-import subway.dto.LineResponse;
-
 import java.net.URI;
 import java.sql.SQLException;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import subway.application.LineService;
+import subway.dto.LineRequest;
+import subway.dto.LineResponse;
+import subway.dto.SectionRequest;
 
 @RestController
 @RequestMapping("/lines")
@@ -45,6 +53,14 @@ public class LineController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
         lineService.deleteLineById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{lineId}/sections")
+    public ResponseEntity<Void> createSection(@PathVariable("lineId") Long lineId,
+            @RequestBody SectionRequest sectionRequest) {
+        
+        lineService.connectSectionByStationId(lineId, sectionRequest);
         return ResponseEntity.noContent().build();
     }
 
