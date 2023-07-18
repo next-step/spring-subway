a12b2ebc3f5eb312fb6eb9a889c8561ecae228e0
devxb <dlwnsdud205@naver.com>
2023-07-18 11:47:56 +0900

    test: Line.addSection은 id가 null인 Section이 들어오면 예외를 던진다

--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -1,6 +1,7 @@
 package subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -33,6 +34,25 @@ class LineTest {
 
             // then
             assertThat(sectionList).contains(section);
+        }
+
+        @Test
+        @DisplayName("id가 null인 Section이 입력된다면, IllegalArgumentException을 던진다")
+        void Throw_IllegalArgumentException_Input_Null_Id_Section() {
+            // given
+            Line line = new Line("line", "red");
+
+            Section section = Section.builder()
+                    .upStation(new Station(2L, "upStation"))
+                    .downStation(new Station(3L, "downStation"))
+                    .distance(10)
+                    .build();
+
+            // when
+            Exception exception = catchException(() -> line.addSection(section));
+
+            // then
+            assertThat(exception).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
