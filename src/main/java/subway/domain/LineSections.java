38f3e8165d18fef445aa1ea6980245f5954e014f
chanwooDev <pove2019@gmail.com>
2023-07-19 14:59:19 +0900

    refactor: LineSections / 현재 구간은 해당 노선에 속하지 않습니다

--- src/main/java/subway/domain/LineSections.java
+++ src/main/java/subway/domain/LineSections.java
@@ -8,11 +8,15 @@ public class LineSections {
     private final Sections sections;
 
     public LineSections(Line line, Section section) {
-        if (!section.belongTo(line)) {
-            throw new IllegalArgumentException("현재 구간은 해당 노선에 속하지 않습니다. current line: " + line + ", section: " + section);
-        }
+        validateSectionBelongToLine(line, section);
 
         this.line = line;
         this.sections = new Sections(List.of(section));
+    }
+
+    private void validateSectionBelongToLine(Line line, Section section) {
+        if (!section.belongTo(line)) {
+            throw new IllegalArgumentException("현재 구간은 해당 노선에 속하지 않습니다. current line: " + line + ", section: " + section);
+        }
     }
 }
