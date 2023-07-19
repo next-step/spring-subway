c136995acdc3a67939eb00adc859908e6bc058a2
whatasame <whatasame@gmail.com>
2023-07-19 16:37:48 +0900

    fix: 잘못된 메서드명 수정
    Co-authored-by: protoseo <proto_seo@naver.com>

--- src/main/java/subway/ui/SectionController.java
+++ src/main/java/subway/ui/SectionController.java
@@ -28,7 +28,7 @@ public class SectionController {
     }
 
     @DeleteMapping("/lines/{lineId}/sections")
-    public ResponseEntity<Void> createSection(
+    public ResponseEntity<Void> deleteSection(
             @PathVariable Long lineId,
             @RequestParam Long stationId
     ) {
