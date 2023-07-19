ca67450d2883b4028b9825cc88c705c1e7fb2a01
chanwooDev <pove2019@gmail.com>
2023-07-19 15:42:11 +0900

    feat: LineRequest / station, distance 필드 추가

--- src/main/java/subway/dto/LineRequest.java
+++ src/main/java/subway/dto/LineRequest.java
@@ -1,7 +1,11 @@
 package subway.dto;
 
 public class LineRequest {
+
     private String name;
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
     private String color;
 
     public LineRequest() {
@@ -12,12 +16,32 @@ public class LineRequest {
         this.color = color;
     }
 
+    public LineRequest(String name, Long upStationId, Long downStationId, Integer distance,
+        String color) {
+        this.name = name;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+        this.color = color;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Integer getDistance() {
+        return distance;
     }
 
     public String getColor() {
         return color;
     }
-
 }
