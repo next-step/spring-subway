32bb73ca1f1d0fcc10b4ed0bb738b3b7d90b5f5f
jjungk <jsk33a@gmail.com>
2023-07-20 14:30:00 +0900

    feat: Json 파싱 예외 처리 코드 추가

--- src/main/java/subway/ui/exception/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package subway.ui.exception;
 
 import static org.springframework.http.HttpStatus.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,5 +20,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ErrorResponse illegalStateExceptionHandle(IllegalStateException exception) {
         return new ErrorResponse(INTERNAL_SERVER_ERROR.value(), exception.getMessage());
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(JsonProcessingException.class)
+    public ErrorResponse jsonProcessingExceptionHandle(JsonProcessingException exception) {
+        return new ErrorResponse(BAD_REQUEST.value(), "입력 값이 잘못되었습니다.");
     }
 }
