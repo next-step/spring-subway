2c4da1c3aa2132933f7060af5fc09b5262b137cd
jjungk <jsk33a@gmail.com>
2023-07-20 16:39:52 +0900

    refactor: ExceptionHandler를 GlobalExceptionHandler로 이동

--- src/main/java/subway/ui/exception/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package subway.ui.exception;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.sql.SQLException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,7 +26,13 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(JsonProcessingException.class)
-    public ErrorResponse jsonProcessingExceptionHandle(JsonProcessingException exception) {
+    public ErrorResponse jsonProcessingExceptionHandle() {
         return new ErrorResponse(BAD_REQUEST.value(), "입력 값이 잘못되었습니다.");
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(SQLException.class)
+    public ErrorResponse handleSQLException() {
+        return new ErrorResponse(BAD_REQUEST.value(), "잘못된 요청 정보 입니다.");
     }
 }
