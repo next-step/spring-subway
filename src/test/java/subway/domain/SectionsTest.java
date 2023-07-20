1b7c0b7ddb3258786c6e5aca0fff4c55ecf04ec0
dongjoo0-0 <kdj486347@gmail.com>
2023-07-20 15:06:40 +0900

    style: 불필요한 import 삭제

--- src/test/java/subway/domain/SectionsTest.java
+++ src/test/java/subway/domain/SectionsTest.java
@@ -1,13 +1,12 @@
 package subway.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SectionsTest {
