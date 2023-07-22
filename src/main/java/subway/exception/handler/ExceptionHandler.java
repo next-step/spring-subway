3eefb9978435ae3183218ed6126a76378643b746
whatasame <whatasame@gmail.com>
2023-07-22 14:10:22 +0900

    refactor: SQLException에 대한 hanlder 추가

--- src/main/java/subway/exception/handler/ExceptionHandler.java
+++ src/main/java/subway/exception/handler/ExceptionHandler.java
@@ -1,18 +1,26 @@
 package subway.exception.handler;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import subway.exception.SubwayException;
 import subway.exception.dto.ExceptionResponse;
 
-@RestControllerAdvice
-public class RuntimeExceptionHandler {
+import java.sql.SQLException;
 
-    @ExceptionHandler(SubwayException.class)
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(SubwayException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleSubwayException(final SubwayException exception) {
         return new ExceptionResponse(exception.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleSQLException(final SQLException exception) {
+        exception.printStackTrace();
+        return new ExceptionResponse("올바르지 않은 데이터 접근입니다.");
     }
 }
