70a4966780e4535687437976a922e259bcb73451
ProtoSeo <proto_seo@naver.com>
2023-07-23 23:42:40 +0900

    refactor: 빈 생성자 접근 제어자 수정 및 설명 주석 추가

--- src/main/java/subway/dto/StationRequest.java
+++ src/main/java/subway/dto/StationRequest.java
@@ -1,9 +1,11 @@
 package subway.dto;
 
 public class StationRequest {
+
     private String name;
 
-    public StationRequest() {
+    private StationRequest() {
+        // 필드가 하나 일 때, 기본 생성자가 없는 경우 직렬화시 오류 발생
     }
 
     public StationRequest(String name) {
