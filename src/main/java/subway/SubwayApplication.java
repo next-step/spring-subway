c9001ccf6d6ce7014a7547520e7d0d058176c0c0
GrolarKim <audwnswmf@naver.com>
2023-07-23 23:53:11 +0900

    refactor: configuration 분리

--- src/main/java/subway/SubwayApplication.java
+++ src/main/java/subway/SubwayApplication.java
@@ -2,9 +2,6 @@ package subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SubwayApplication {
@@ -13,15 +10,4 @@ public class SubwayApplication {
         SpringApplication.run(SubwayApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*");
-            }
-        };
-    }
 }
