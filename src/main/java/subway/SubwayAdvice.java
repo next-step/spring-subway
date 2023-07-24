d1b1fd70b2c29c796fe48b5076bedce8f7dd6152
dongjoo0-0 <kdj486347@gmail.com>
2023-07-24 17:12:46 +0900

    fix: 프론트에 에러 메시지 전달하도록 수정

--- src/main/java/subway/SubwayAdvice.java
+++ src/main/java/subway/SubwayAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import subway.dto.ErrorResponse;
 import subway.exception.IncorrectRequestException;
 import subway.exception.InternalStateException;
@@ -11,23 +12,27 @@ import subway.exception.InternalStateException;
 import java.sql.SQLException;
 
 @RestControllerAdvice
-public class SubwayAdvice {
+public class SubwayAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<ErrorResponse> handleSQLException(SQLException e) {
         ErrorResponse response = new ErrorResponse("Invalid SQL Request.");
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+        return handleExceptionInternal(response, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(IncorrectRequestException.class)
     public ResponseEntity<ErrorResponse> handleIncorrectRequestException(IncorrectRequestException e) {
         ErrorResponse response = new ErrorResponse(e.getMessage());
-        return ResponseEntity.badRequest().body(response);
+        return handleExceptionInternal(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InternalStateException.class)
     public ResponseEntity<ErrorResponse> handleInternalStateException(InternalStateException e) {
         ErrorResponse response = new ErrorResponse(e.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+        return handleExceptionInternal(response, HttpStatus.CONFLICT);
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorResponse response, HttpStatus status) {
+        return ResponseEntity.status(status).body(response);
     }
 }
