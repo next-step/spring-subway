32e04f5bcfc478d8b8b2ce7988202c4fe936d6a2
dahyen0o <seeunchoi99@gmail.com>
2023-07-24 20:26:16 +0900

    refactor: StationIntegrationSupporter를 사용해 StationIntegrationTest 리팩토링

--- src/test/java/subway/integration/StationIntegrationSupporter.java
+++ src/test/java/subway/integration/StationIntegrationSupporter.java
@@ -18,4 +18,39 @@ public class StationIntegrationSupporter {
                 .then().log().all()
                 .extract();
     }
+
+
+    static ExtractableResponse<Response> findAllStations() {
+        return given().log().all()
+                .when()
+                .get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> findStation(Long stationId) {
+        return given().log().all()
+                .when()
+                .get("/stations/{stationId}", stationId)
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> updateStation(String uri, StationRequest stationRequest) {
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(stationRequest)
+                .when()
+                .put(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> deleteStation(String uri) {
+        return given().log().all()
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
 }
