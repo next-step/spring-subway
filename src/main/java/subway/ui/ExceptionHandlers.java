d3c8801ab4b2606881a566e07fe8a8faa497ef2b
김명준 <audwnswmf@naver.com>
2023-07-24 16:13:33 +0900

    feat: spring-boot-validation 추가

--- src/main/java/subway/ui/ExceptionHandlers.java
+++ src/main/java/subway/ui/ExceptionHandlers.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import subway.dto.ErrorResponse;
@@ -17,22 +18,31 @@ public class ExceptionHandlers {
 
     @ExceptionHandler(SubwayBaseException.class)
     public ResponseEntity<ErrorResponse> handleSubwayException(SubwayBaseException exception) {
-        log.error(exception.getLocalizedMessage());
+        log.error(exception.getMessage());
         return ResponseEntity.status(exception.getCode()).body(new ErrorResponse(exception));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+
+        log.error(exception.getMessage());
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
             IllegalArgumentException exception) {
-        
-        log.error(exception.getLocalizedMessage());
+
+        log.error(exception.getMessage());
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
     }
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<Void> handleSQLException(SQLException sqlException) {
-        log.error(sqlException.getLocalizedMessage());
+        log.error(sqlException.getMessage());
         return ResponseEntity.badRequest().build();
     }
 
