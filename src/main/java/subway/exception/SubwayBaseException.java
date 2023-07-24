bd3aaeacbbffe0a7ff17e884beb5c7c33c9d2820
김명준 <audwnswmf@naver.com>
2023-07-24 13:36:14 +0900

    feat: SubwayBaseException 추가

---
+++ src/main/java/subway/exception/SubwayBaseException.java
@@ -0,0 +1,8 @@
+package subway.exception;
+
+public abstract class SubwayBaseException extends RuntimeException {
+
+    public SubwayBaseException(String message) {
+        super(message);
+    }
+}
