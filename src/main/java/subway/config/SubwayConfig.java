7792af3247c4f1b4c1d4de6fea1d2d136f0e0e38
dahyen0o <seeunchoi99@gmail.com>
2023-07-24 19:44:02 +0900

    refactor: corsConfigurer 분리

--- src/main/java/subway/config/SubwayConfig.java
+++ src/main/java/subway/config/SubwayConfig.java
@@ -1,14 +1,22 @@
 package subway.config;
 
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SubwayConfig {
 
-//    @Bean
-//    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-//        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
