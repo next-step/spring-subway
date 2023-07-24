ef495cbcf2a3dea215b38ff60820362760d35fdb
pushedrumex <pushedrumex@naver.com>
2023-07-24 20:26:32 +0900

    feat: SubwayException과 ErrorResponse를 생성한다

---
+++ src/main/java/subway/exception/StationException.java
@@ -0,0 +1,8 @@
+package subway.exception;
+
+public class StationException extends SubwayException {
+
+    public StationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/subway/exception/SubwayException.java
@@ -0,0 +1,8 @@
+package subway.exception;
+
+public abstract class SubwayException extends RuntimeException {
+
+    public SubwayException(String message) {
+        super(message);
+    }
+}
