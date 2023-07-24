4b2ea5ce8a8c5a8840eb22f358610e78b1abbc02
dongjoo0-0 <kdj486347@gmail.com>
2023-07-24 16:30:42 +0900

    test: 구간 관련 통합테스트 통일 및 노선 제거 추가 테스트 작성

--- src/test/java/subway/integration/SectionIntegrationTest.java
+++ src/test/java/subway/integration/SectionIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import subway.dto.SectionRequest;
 import subway.helper.CreateHelper;
+import subway.helper.GetHelper;
 
 import java.util.List;
 
@@ -21,12 +22,9 @@ class SectionIntegrationTest extends IntegrationTest {
     @Test
     void createDownwardSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(1), stationIds.get(2), 10);
+        SectionRequest params = new SectionRequest(2L, 3L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -39,18 +37,17 @@ class SectionIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("강남역", "역삼역", "교대역");
     }
 
     @DisplayName("상행 종점역을 연장하는 지하철 구간을 생성한다.")
     @Test
     void createUpwardSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(2), stationIds.get(0), 10);
+        SectionRequest params = new SectionRequest(3L, 1L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -63,18 +60,17 @@ class SectionIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("교대역", "강남역", "역삼역");
     }
 
     @DisplayName("상행역을 기준으로 중간에 역을 삽입하여 지하철 구간을 생성한다.")
     @Test
     void createUpInsertSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(0), stationIds.get(2), 5);
+        SectionRequest params = new SectionRequest(1L, 3L, 5);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -87,18 +83,17 @@ class SectionIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("강남역", "교대역", "역삼역");
     }
 
     @DisplayName("하행역을 기준으로 중간에 역을 삽입하여 지하철 구간을 생성한다.")
     @Test
     void createDownInsertSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(2), stationIds.get(1), 5);
+        SectionRequest params = new SectionRequest(3L, 2L, 5);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -111,17 +106,16 @@ class SectionIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("강남역", "교대역", "역삼역");
     }
 
     @DisplayName("기존에 존재하는 지하철 역으로 지하철 구간을 생성한다.")
     @Test
     void createAlreadyExist() {
         // given
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(0), stationIds.get(1), 10);
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
+        SectionRequest params = new SectionRequest(1L, 2L, 10);
         RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -130,7 +124,7 @@ class SectionIntegrationTest extends IntegrationTest {
                 .then().log().all()
                 .extract();
 
-        SectionRequest params2 = new SectionRequest(stationIds.get(1), stationIds.get(0), 10);
+        SectionRequest params2 = new SectionRequest(2L, 1L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -149,13 +143,10 @@ class SectionIntegrationTest extends IntegrationTest {
     @Test
     void createNotExist() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
         CreateHelper.createStation("선릉역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(2), stationIds.get(3), 10);
+        SectionRequest params = new SectionRequest(3L, 4L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -174,11 +165,8 @@ class SectionIntegrationTest extends IntegrationTest {
     @Test
     void createSame() {
         // given
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(0), stationIds.get(0), 10);
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
+        SectionRequest params = new SectionRequest(1L, 1L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -192,18 +180,14 @@ class SectionIntegrationTest extends IntegrationTest {
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
-
 
     @DisplayName("중간에 기존 구간보다 길이가 긴 지하철 구간을 생성한다.")
     @Test
     void createTooLongSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("교대역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-
-        SectionRequest params = new SectionRequest(stationIds.get(0), stationIds.get(2), 10);
+        SectionRequest params = new SectionRequest(1L, 3L, 10);
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -218,45 +202,80 @@ class SectionIntegrationTest extends IntegrationTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
-    @DisplayName("지하철 구간을 제거한다.")
+    @DisplayName("종점역 지하철 구간을 제거한다.")
     @Test
     void deleteSection() {
         // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
         CreateHelper.createStation("잠실역");
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-        CreateHelper.createSection(stationIds, 0, 1, lineId);
-        CreateHelper.createSection(stationIds, 1, 2, lineId);
+        CreateHelper.createSection(2L, 3L, lineId);
 
         // when
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
-                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, stationIds.get(2))
+                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, 3L)
                 .then().log().all()
                 .extract();
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("강남역", "역삼역");
     }
 
-    @DisplayName("마지막 남은 지하철 구간을 제거한다.")
+    @DisplayName("중간에 있는 지하철 구간을 제거한다.")
     @Test
-    void deletelastSection() {
+    void deleteMiddleStation() {
         // given
-        Long lineId = CreateHelper.createLine("2호선", "bg-123");
-
-        List<Long> stationIds = CreateHelper.getStationIds();
-        CreateHelper.createSection(stationIds, 0, 1, lineId);
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
+        CreateHelper.createStation("잠실역");
+        CreateHelper.createSection(2L, 3L, lineId);
 
         // when
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
-                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, stationIds.get(1))
+                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, 2L)
                 .then().log().all()
                 .extract();
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CONFLICT.value());
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        List<String> stationNames = GetHelper.getStationNamesInLine(lineId);
+        assertThat(stationNames).containsExactly("강남역", "잠실역");
+    }
+
+    @DisplayName("마지막 남은 지하철 구간을 제거한다.")
+    @Test
+    void deleteLastSection() {
+        // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, 2L)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("기존에 존재하지 않는 역으로 구간을 제거한다.")
+    @Test
+    void deleteNotExist() {
+        // given
+        Long lineId = CreateHelper.createLine("2호선", "bg-123", "강남역", "역삼역");
+        CreateHelper.createStation("잠실역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when().delete("/lines/{lineId}/sections?stationId={stationId}", lineId, 3L)
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
