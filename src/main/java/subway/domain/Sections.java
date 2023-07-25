5e23083e5d3f0c3c17de4598bfeba13ea9fc2357
bkRyusim <bkryusim@gmail.com>
2023-07-25 17:17:20 +0900

    refactor: Sections 클래스 내부에서 캐싱처리

--- src/main/java/subway/domain/Sections.java
+++ src/main/java/subway/domain/Sections.java
@@ -3,19 +3,25 @@ package subway.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Sections {
     private final List<Section> sections;
+    private final Set<Station> downStationsCache;
+    private final Set<Station> upStationsCache;
 
     public Sections(final List<Section> sections) {
         this.sections = Collections.unmodifiableList(sections);
+        this.downStationsCache = sections.stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toSet());
+        this.upStationsCache = sections.stream()
+                .map(Section::getUpStation)
+                .collect(Collectors.toSet());
     }
 
     public Sections() {
@@ -50,11 +56,7 @@ public class Sections {
     }
 
     private boolean contains(final Station station) {
-        return sections.stream()
-                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
-                .distinct()
-                .collect(Collectors.toList())
-                .contains(station);
+        return downStationsCache.contains(station) || upStationsCache.contains(station);
     }
 
     private boolean notContains(final Station station) {
@@ -85,17 +87,17 @@ public class Sections {
     }
 
     private boolean isTerminalDownStation(final Station station) {
-        Set<Station> upStations = sections.stream()
-                .map(Section::getUpStation)
-                .collect(Collectors.toSet());
-
-        Station terminal = sections.stream()
-                .map(Section::getDownStation)
-                .filter(downStation -> !upStations.contains(downStation))
-                .findAny()
-                .orElseThrow(() -> new IllegalStateException("하행 종점이 존재하지 않습니다."));
+        Station terminal = findTerminalDownStation();
 
         return station.equals(terminal);
+    }
+
+    private Station findTerminalDownStation() {
+        return sections.stream()
+                .map(Section::getDownStation)
+                .filter(downStation -> !upStationsCache.contains(downStation))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("하행 종점이 존재하지 않습니다."));
     }
 
     private void validateSize() {
@@ -109,13 +111,7 @@ public class Sections {
 
         sections.forEach(section -> stationMap.put(section.getUpStation(), section.getDownStation()));
 
-        Set<Station> downStations = new HashSet<>(stationMap.values());
-
-        Station start = sections.stream()
-                .map(Section::getUpStation)
-                .filter(downStation -> !downStations.contains(downStation))
-                .findAny()
-                .orElse(null);
+        Station start = findTerminalUpStation();
 
         List<Station> sortedStations = new ArrayList<>();
 
@@ -125,6 +121,15 @@ public class Sections {
         }
 
         return sortedStations;
+    }
+
+    private Station findTerminalUpStation() {
+        Station start = sections.stream()
+                .map(Section::getUpStation)
+                .filter(downStation -> !downStationsCache.contains(downStation))
+                .findAny()
+                .orElse(null);
+        return start;
     }
 
     public List<Section> getSections() {
