8646b692ecf8123bc97112c0ef9f9a9e68a42ba2
jiwon choi <jjiwoning12@gmail.com>
2023-07-25 15:31:08 +0900

    refactor: 예외 핸들러 던지는 예외 수정

--- src/main/java/subway/ui/exception/GlobalExceptionHandler.java
+++ src/main/java/subway/ui/exception/GlobalExceptionHandler.java
@@ -35,12 +35,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public ErrorResponse uniqueExceptionHandle() {
-        return new ErrorResponse(BAD_REQUEST.value(), "중복된 이름을 입력했습니다.");
+        return new ErrorResponse(BAD_REQUEST.value(), "중복된 이름을 입력했거나 값이 비어있습니다.");
     }
 
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(SQLException.class)
     public ErrorResponse handleSQLException() {
-        return new ErrorResponse(BAD_REQUEST.value(), "잘못된 요청 정보 입니다.");
+        return new ErrorResponse(INTERNAL_SERVER_ERROR.value(), "현재 서버에 문제가 있습니다.");
     }
 }
