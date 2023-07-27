8b3cde6b433d5eafba6f66d8a06f79e5572b1c5a
hwihwi <yjh200523@naver.com>
2023-07-28 02:28:27 +0900

    refactor: RestAssured 로직을 기존에 분리해 둔 메서드로 변경하여 가독성 개선

--- src/test/java/subway/helper/CreateHelper.java
+++ src/test/java/subway/helper/CreateHelper.java
@@ -1,11 +1,9 @@
 package subway.helper;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.http.MediaType;
 import subway.dto.LineRequest;
 import subway.dto.LineResponse;
 import subway.dto.LineStationsResponse;
@@ -15,48 +13,31 @@ import subway.dto.StationResponse;
 public class CreateHelper {
 
     public static int getStationCountInLine(final Long lineId) {
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/lines/" + lineId)
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> response = RestAssuredHelper.get("/lines/" + lineId);
+
         return response.jsonPath().getObject(".", LineStationsResponse.class)
                 .getStations().size();
     }
 
     public static void createSection(final Long upStationId, final Long downStationId, Long lineId) {
         SectionRequest params = new SectionRequest(upStationId, downStationId, 10);
-        RestAssured.given()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/lines/{id}/sections", lineId)
-                .then()
-                .extract();
+
+        RestAssuredHelper.post("/lines/" + lineId + "/sections", params);
     }
 
     public static Long createLine(String name, String color, long upStationId, long downStationId) {
         LineRequest lineRequest = new LineRequest(name, color, upStationId, downStationId, 10);
-        ExtractableResponse<Response> lineResponse = RestAssured.given()
-                .body(lineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/lines")
-                .then()
-                .extract();
+
+        ExtractableResponse<Response> lineResponse = RestAssuredHelper.post("/lines", lineRequest);
+
         return lineResponse.as(LineResponse.class).getId();
     }
 
     public static Long createStation(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/stations")
-                .then().log().all()
-                .extract();
+
+        ExtractableResponse<Response> response = RestAssuredHelper.post("/stations", params);
 
         return response.as(StationResponse.class).getId();
     }
