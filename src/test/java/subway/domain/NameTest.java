bc0fb5ace094bee831686501b7973d86eda9e8a1
hwihwi <yjh200523@naver.com>
2023-07-27 12:16:41 +0900

    test: Name을 LineName, StationName으로 분리하고 각각의 테스트 작성

--- src/test/java/subway/domain/NameTest.java
+++ src/test/java/subway/domain/NameTest.java
@@ -10,21 +10,39 @@ import subway.exception.InvalidRequestException;
 
 class NameTest {
 
-    @DisplayName("지하철 역명이나 노선명에 사용할 이름 생성")
+    @DisplayName("지하철 역명으로 사용할 이름 생성")
     @ParameterizedTest
     @ValueSource(strings = {"일", "일이", "일이삼"})
-    void createName(String name) {
-        assertDoesNotThrow(() -> new Name(name));
+    void createStationName(String name) {
+        assertDoesNotThrow(() -> new StationName(name));
     }
 
-    @DisplayName("이름 길이 제한을 초과하는 경우 예외를 던진다.")
+    @DisplayName("지하철 역명이 길이 제한을 초과하는 경우 예외를 던진다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 256})
-    void outOfRangeNameLength(int length) {
+    void outOfRangeStationNameLength(int length) {
         // given
         final String name = "가".repeat(length);
 
         // when & then
-        assertThrows(InvalidRequestException.class, () -> new Name(name));
+        assertThrows(InvalidRequestException.class, () -> new StationName(name));
+    }
+
+    @DisplayName("노선명으로 사용할 이름 생성")
+    @ParameterizedTest
+    @ValueSource(strings = {"일", "일이", "일이삼"})
+    void createLineName(String name) {
+        assertDoesNotThrow(() -> new LineName(name));
+    }
+
+    @DisplayName("노선명이 길이 제한을 초과하는 경우 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 256})
+    void outOfRangeLineNameLength(int length) {
+        // given
+        final String name = "가".repeat(length);
+
+        // when & then
+        assertThrows(InvalidRequestException.class, () -> new LineName(name));
     }
 }
