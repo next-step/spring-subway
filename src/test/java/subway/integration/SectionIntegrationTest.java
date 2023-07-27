7b70cd77ba160bf4570e3a852658ab96602de3d1
jjungk <jsk33a@gmail.com>
2023-07-27 19:35:04 +0900

    refactor: SectionIntegrationTest Testfixture로 분리

--- src/test/java/subway/integration/SectionIntegrationTest.java
+++ src/test/java/subway/integration/SectionIntegrationTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,8 +19,6 @@ import subway.dto.request.SectionRegistRequest;
 @DisplayName("지하철 구간 관련 기능")
 public class SectionIntegrationTest extends IntegrationTest {
 
-    private SectionRegistRequest sectionRegistRequest1;
-    private SectionRegistRequest sectionRegistRequest2;
     @Autowired
     private SectionDao sectionDao;
 
@@ -26,18 +26,59 @@ public class SectionIntegrationTest extends IntegrationTest {
     public void setUp() {
         super.setUp();
 
-        sectionRegistRequest1 = new SectionRegistRequest(2L, 3L, 10);
-        sectionRegistRequest2 = new SectionRegistRequest(3L, 4L, 10);
+        SectionRegistRequest 구간_신중동역_춘의역 = new SectionRegistRequest(2L, 3L, 10);
+        SectionRegistRequest 구간_춘의역_부천종합운동장역 = new SectionRegistRequest(3L, 4L, 10);
+
+        Map<String, String> 온수역_등록_요청 = new HashMap<>();
+        온수역_등록_요청.put("name", "온수역");
+        RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(온수역_등록_요청)
+            .when().post("/stations")
+            .then().log().all();
+        RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(구간_신중동역_춘의역)
+            .when().post("/lines/1/sections")
+            .then().log().all();
+        RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(구간_춘의역_부천종합운동장역)
+            .when().post("/lines/1/sections")
+            .then().log().all();
     }
 
     @Test
     @DisplayName("구간 등록 성공 테스트")
     void create() {
         // when
+        SectionRegistRequest 구간_춘의역_온수역 = new SectionRegistRequest(3L, 5L, 9);
+
         ExtractableResponse<Response> response = RestAssured
             .given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest1)
+            .body(구간_춘의역_온수역)
+            .when().post("/lines/1/sections")
+            .then().log().all()
+            .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("구간 등록 성공 테스트")
+    void registerSectionInMiddle() {
+        // when
+        SectionRegistRequest 구간_부천종합운동장역_온수역 = new SectionRegistRequest(4L, 5L, 10);
+
+        ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(구간_부천종합운동장역_온수역)
             .when().post("/lines/1/sections")
             .then().log().all()
             .extract();
@@ -49,20 +90,6 @@ public class SectionIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("구간 상행 종점역 제거 테스트")
     void deleteUpPointStation() {
-        // given
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest1)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest2)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-
         // when
         ExtractableResponse<Response> result = RestAssured
             .given().log().all()
@@ -79,20 +106,6 @@ public class SectionIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("구간 하행 종점역 제거 테스트")
     void deleteDownPointStation() {
-        // given
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest1)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest2)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-
         // when
         ExtractableResponse<Response> result = RestAssured
             .given().log().all()
@@ -110,20 +123,6 @@ public class SectionIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("구간 중간역 제거 테스트")
     void deleteMiddleStation() {
-        // given
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest1)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-        RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(sectionRegistRequest2)
-            .when().post("/lines/1/sections")
-            .then().log().all();
-
         // when
         ExtractableResponse<Response> result = RestAssured
             .given().log().all()
