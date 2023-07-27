95a4717b31877770bf1de75b65e810d37a4f78b4
dongjoo0-0 <kdj486347@gmail.com>
2023-07-27 14:07:01 +0900

    test: 새로운 경로 테스트 관련 통합테스트 작성

---
+++ src/main/java/subway/dto/PathResponse.java
@@ -0,0 +1,24 @@
+package subway.dto;
+
+import subway.domain.Station;
+
+import java.util.List;
+
+public class PathResponse {
+
+    private List<Station> stations;
+    private int distance;
+
+    public PathResponse(List<Station> stations, int distance) {
+        this.stations = stations;
+        this.distance = distance;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
