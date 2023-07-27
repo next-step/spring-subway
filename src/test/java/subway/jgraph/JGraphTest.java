503eda6c90683d7675c08cca7b765559634521db
jiwon choi <jjiwoning12@gmail.com>
2023-07-27 14:27:57 +0900

    test: JGraph 학습 테스트

---
+++ src/test/java/subway/jgraph/JGraphTest.java
@@ -0,0 +1,33 @@
+package subway.jgraph;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JGraphTest {
+
+    @Test
+    @DisplayName("JGraph 학습 테스트")
+    void jGraph() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+            = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        DijkstraShortestPath dijkstraShortestPath
+            = new DijkstraShortestPath(graph);
+        List<String> shortestPath
+            = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
+}
