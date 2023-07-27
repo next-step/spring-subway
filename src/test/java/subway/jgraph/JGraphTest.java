693a77f2baa21f23ecc1f7db92d2be5973e0c693
jiwon choi <jjiwoning12@gmail.com>
2023-07-27 14:35:54 +0900

    test: JGraph 학습 테스트 예외 상황 추가

--- src/test/java/subway/jgraph/JGraphTest.java
+++ src/test/java/subway/jgraph/JGraphTest.java
@@ -1,6 +1,7 @@
 package subway.jgraph;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -28,6 +29,30 @@ public class JGraphTest {
         List<String> shortestPath
             = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
 
+        double pathWeight = dijkstraShortestPath.getPathWeight("v3", "v1");
+
         assertThat(shortestPath.size()).isEqualTo(3);
+        assertThat(pathWeight).isEqualTo(4.0);
+    }
+
+    @Test
+    @DisplayName("JGraph 학습 테스트: 출발지와 도착지가 연결되지 않은 경우")
+    void jGraphException() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph
+            = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.addVertex("v4");
+        graph.addVertex("v5");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+        graph.setEdgeWeight(graph.addEdge("v4", "v5"), 2);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+
+        assertThatThrownBy(() -> dijkstraShortestPath.getPath("v4", "v1").getVertexList())
+            .isInstanceOf(NullPointerException.class);
     }
 }
