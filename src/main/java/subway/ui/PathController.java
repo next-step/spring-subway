1e003548396ed97679b56b1d9844d1abd71221d5
gunner6603 <gunner6603@snu.ac.kr>
2023-07-28 15:48:07 +0900

    feat: PathController / 메서드 추가

---
+++ src/main/java/subway/ui/PathController.java
@@ -0,0 +1,27 @@
+package subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import subway.application.SectionService;
+import subway.domain.ShortestPath;
+import subway.dto.PathRequest;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+
+    private final SectionService sectionService;
+
+    public PathController(final SectionService sectionService) {
+        this.sectionService = sectionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ShortestPath> findPath(@ModelAttribute PathRequest pathRequest) {
+        final ShortestPath shortestPath = sectionService.findShortestPath(pathRequest);
+        return ResponseEntity.ok().body(shortestPath);
+    }
+}
