33f76a74bf590b63cb4f4d66b562aa1af7a4183a
dahyen0o <seeunchoi99@gmail.com>
2023-07-28 16:02:50 +0900

    refactor: SectionTest의 테스트 변수들 한글로 변경

--- src/test/java/subway/domain/SectionTest.java
+++ src/test/java/subway/domain/SectionTest.java
@@ -3,46 +3,49 @@ package subway.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.exception.IllegalSectionException;
+import subway.fixture.LineFixture;
+import subway.fixture.SectionFixture;
+import subway.fixture.StationFixture;
 
 import static org.assertj.core.api.Assertions.*;
 
 class SectionTest {
 
-    @DisplayName("생성 테스트")
+    @DisplayName("section 생성에 성공한다.")
     @Test
     void createSectionTest() {
         // given
-        final Line line = new Line(1L, "4호선", "blue");
-        final Station station1 = new Station(1L, "오이도");
-        final Station station2 = new Station(2L, "안산");
+        final Line 신분당선 = LineFixture.신분당선();
+        final Station 첫번째역 = StationFixture.첫번째역();
+        final Station 두번째역 = StationFixture.두번째역();
 
         // when & then
-        assertThatNoException().isThrownBy(() -> new Section(line, station1, station2, 10));
+        assertThatNoException().isThrownBy(() -> new Section(신분당선, 첫번째역, 두번째역, SectionFixture.DEFAULT_DISTANCE));
     }
 
-    @DisplayName("길이 유효성 검증 테스트")
+    @DisplayName("0 이하인 길이로 section 생성에 실패한다.")
     @Test
     void validateDistanceTest() {
         // given
-        final Line line = new Line(1L, "4호선", "blue");
-        final Station station1 = new Station(1L, "오이도");
-        final Station station2 = new Station(2L, "안산");
+        final Line 신분당선 = LineFixture.신분당선();
+        final Station 첫번째역 = StationFixture.첫번째역();
+        final Station 두번째역 = StationFixture.두번째역();
 
         // when & then
-        assertThatThrownBy(() -> new Section(line, station1, station2, 0))
+        assertThatThrownBy(() -> new Section(신분당선, 첫번째역, 두번째역, 0))
                 .hasMessage("구간 길이는 0보다 커야합니다.")
                 .isInstanceOf(IllegalSectionException.class);
     }
 
-    @DisplayName("상행 및 하행역 유효성 검증 테스트")
+    @DisplayName("상행 및 하행역이 같아 section 생성에 실패한다.")
     @Test
     void validateStationsTest() {
         // given
-        final Line line = new Line(1L, "4호선", "blue");
-        final Station station = new Station(1L, "오이도");
+        final Line 신분당선 = LineFixture.신분당선();
+        final Station 첫번째역 = StationFixture.첫번째역();
 
         // when & then
-        assertThatThrownBy(() -> new Section(line, station, station, 0))
+        assertThatThrownBy(() -> new Section(신분당선, 첫번째역, 첫번째역, SectionFixture.DEFAULT_DISTANCE))
                 .hasMessage("상행역과 하행역은 같을 수 없습니다.")
                 .isInstanceOf(IllegalSectionException.class);
     }
