01ae56c6cd17030918dc80320c6c634d856d0303
impati <yongs170@naver.com>
2023-07-29 16:21:28 +0900

    feat: 경로 조회 기능 컨트롤러 구현

---
+++ src/main/java/subway/ui/PathController.java
@@ -0,0 +1,27 @@
+package subway.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import subway.application.PathFinder;
+import subway.dto.request.PathFindRequest;
+import subway.dto.response.PathFindResponse;
+
+@RestController
+@RequestMapping("/paths")
+public class PathController {
+
+    private final PathFinder pathFinder;
+
+    public PathController(final PathFinder pathFinder) {
+        this.pathFinder = pathFinder;
+    }
+
+    @GetMapping
+    public ResponseEntity<PathFindResponse> find(@RequestParam("source") Long source, @RequestParam("target") Long target) {
+        PathFindResponse response = pathFinder.findShortPath(new PathFindRequest(source, target));
+        return ResponseEntity.ok(response);
+    }
+}
