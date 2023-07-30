de07c1dc2e5364ac411bcee96d610929afab5936
hwihwi <yjh200523@naver.com>
2023-07-30 17:48:22 +0900

    feat: 경로 검색 결과를 저장할 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,3 +25,25 @@ create table if not exists SECTION
     foreign key(down_station_id) references STATION(id),
     foreign key(line_id) references LINE(id) on delete cascade
 );
+
+create table if not exists PATH
+(
+    id bigint auto_increment not null,
+    departure_id bigint not null,
+    arrival_id bigint not null,
+    distance int not null,
+    primary key(id),
+    foreign key(departure_id) references STATION(id),
+    foreign key(arrival_id) references STATION(id)
+    );
+
+create table if not exists DETAIL_PATH
+(
+    id bigint auto_increment not null,
+    path_id bigint not null,
+    waypoint_id bigint not null,
+    sequence int not null,
+    primary key(id),
+    foreign key(path_id) references PATH(id) on delete cascade,
+    foreign key(waypoint_id) references STATION(id)
+    );
