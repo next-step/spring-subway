f83609b5634fe97bd25e03a93c6f4a4b4f95f888
dahyen0o <seeunchoi99@gmail.com>
2023-07-30 21:23:33 +0900

    test: PathServiceTest 실패 테스트

--- src/test/java/subway/application/PathServiceTest.java
+++ src/test/java/subway/application/PathServiceTest.java
@@ -10,12 +10,14 @@ import subway.dao.SectionDao;
 import subway.dao.StationDao;
 import subway.domain.Station;
 import subway.dto.PathResponse;
+import subway.exception.SubwayException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static subway.fixture.SectionFixture.*;
 import static subway.fixture.StationFixture.*;
@@ -70,5 +72,96 @@ class PathServiceTest {
                 .map(stationResponse -> new Station(stationResponse.getId(), stationResponse.getName()))
                 .collect(Collectors.toUnmodifiableList());
         assertThat(stations).containsExactly(범계역, 경마공원역(), 사당역(), 강남역(), 잠실역);
+    }
+
+    @DisplayName("출발역 id 또는 도착역 id가 존재하지 않아 경로를 조회하는 데 실패한다.")
+    @Test
+    void findPathWithStationNoExist() {
+        // given
+        final Station 범계역 = 범계역();
+        final Station 첫번째역 = 첫번째역();
+
+        given(stationDao.findAll()).willReturn(List.of(
+                범계역(), 경마공원역(), 사당역(), 신용산역(), 강남역(), 잠실역(), 여의도역(), 노량진역()
+        ));
+
+        given(sectionDao.findAll()).willReturn(List.of(
+                범계역_경마공원역_구간(DEFAULT_DISTANCE),
+                경마공원역_사당역_구간(DEFAULT_DISTANCE),
+                사당역_신용산역_구간(DEFAULT_DISTANCE),
+                경마공원역_강남역_구간(DEFAULT_DISTANCE * 5),
+                사당역_강남역_구간(DEFAULT_DISTANCE),
+                강남역_잠실역_구간(DEFAULT_DISTANCE),
+                여의도역_노량진역_구간(DEFAULT_DISTANCE)
+        ));
+
+        given(stationDao.findById(범계역.getId())).willReturn(Optional.of(범계역));
+        given(stationDao.findById(첫번째역.getId())).willReturn(Optional.of(첫번째역));
+
+        // when & then
+        assertThatThrownBy(() -> pathService.findPath(범계역.getId(), 첫번째역.getId()))
+                .hasMessage(첫번째역().getName() + "은(는) 존재하지 않는 역입니다.")
+                .isInstanceOf(SubwayException.class);
+        assertThatThrownBy(() -> pathService.findPath(첫번째역.getId(), 범계역.getId()))
+                .hasMessage(첫번째역().getName() + "은(는) 존재하지 않는 역입니다.")
+                .isInstanceOf(SubwayException.class);
+    }
+
+    @DisplayName("출발역 id와 도착역 id가 일치해 경로를 조회하는 데 실패한다.")
+    @Test
+    void findPathWithSameStationId() {
+        // given
+        final Station 범계역 = 범계역();
+
+        given(stationDao.findAll()).willReturn(List.of(
+                범계역(), 경마공원역(), 사당역(), 신용산역(), 강남역(), 잠실역(), 여의도역(), 노량진역()
+        ));
+
+        given(sectionDao.findAll()).willReturn(List.of(
+                범계역_경마공원역_구간(DEFAULT_DISTANCE),
+                경마공원역_사당역_구간(DEFAULT_DISTANCE),
+                사당역_신용산역_구간(DEFAULT_DISTANCE),
+                경마공원역_강남역_구간(DEFAULT_DISTANCE * 5),
+                사당역_강남역_구간(DEFAULT_DISTANCE),
+                강남역_잠실역_구간(DEFAULT_DISTANCE),
+                여의도역_노량진역_구간(DEFAULT_DISTANCE)
+        ));
+
+        given(stationDao.findById(범계역.getId())).willReturn(Optional.of(범계역));
+
+        // when & then
+        assertThatThrownBy(() -> pathService.findPath(범계역.getId(), 범계역.getId()))
+                .hasMessage("출발역과 도착역은 같을 수 없습니다.")
+                .isInstanceOf(SubwayException.class);
+    }
+
+    @DisplayName("출발역과 도착역을 잇는 경로가 존재하지 않아 경로를 조회하는 데 실패한다.")
+    @Test
+    void findPathWithNotConnectedStations() {
+        // given
+        final Station 범계역 = 범계역();
+        final Station 노량진역 = 노량진역();
+
+        given(stationDao.findAll()).willReturn(List.of(
+                범계역(), 경마공원역(), 사당역(), 신용산역(), 강남역(), 잠실역(), 여의도역(), 노량진역()
+        ));
+
+        given(sectionDao.findAll()).willReturn(List.of(
+                범계역_경마공원역_구간(DEFAULT_DISTANCE),
+                경마공원역_사당역_구간(DEFAULT_DISTANCE),
+                사당역_신용산역_구간(DEFAULT_DISTANCE),
+                경마공원역_강남역_구간(DEFAULT_DISTANCE * 5),
+                사당역_강남역_구간(DEFAULT_DISTANCE),
+                강남역_잠실역_구간(DEFAULT_DISTANCE),
+                여의도역_노량진역_구간(DEFAULT_DISTANCE)
+        ));
+
+        given(stationDao.findById(범계역.getId())).willReturn(Optional.of(범계역));
+        given(stationDao.findById(노량진역.getId())).willReturn(Optional.of(노량진역));
+
+        // when & then
+        assertThatThrownBy(() -> pathService.findPath(범계역.getId(), 노량진역.getId()))
+                .hasMessage("출발역과 도착역을 연결하는 경로가 존재하지 않습니다.")
+                .isInstanceOf(SubwayException.class);
     }
 }
