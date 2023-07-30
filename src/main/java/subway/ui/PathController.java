cd176edc6cb061294767ec372193294c73dedbf7
hwihwi <yjh200523@naver.com>
2023-07-31 00:30:17 +0900

    refactor: 출발역과 도착역이 같은지 검증하는 로직을 컨트롤러로 이관

--- src/main/java/subway/ui/PathController.java
+++ src/main/java/subway/ui/PathController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import subway.application.PathService;
 import subway.dto.PathResponse;
+import subway.exception.ErrorCode;
+import subway.exception.FindPathException;
 
 @RestController
 @RequestMapping("/paths")
@@ -24,6 +26,9 @@ public class PathController {
     public ResponseEntity<PathResponse> findShortestPath(@RequestParam String source, @RequestParam String target) {
         if (!source.matches(NUMBER_PATTERN) || !target.matches(NUMBER_PATTERN)) {
             throw new IllegalArgumentException("출발역과 도착역은 정수만 입력할 수 있습니다.");
+        }
+        if (source.equals(target)) {
+            throw new FindPathException(ErrorCode.SAME_SOURCE_AS_TARGET, "출발역과 도착역이 같습니다.");
         }
 
         return ResponseEntity.ok(pathService.findShortestPath(Long.parseLong(source), Long.parseLong(target)));
