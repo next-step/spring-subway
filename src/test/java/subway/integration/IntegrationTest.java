4a89cc6d62c2d570ac583ca2d18ce7c2fb3918e2
rimrim990 <lim990718@naver.com>
2023-07-30 15:08:38 +0900

    refactor: @DirtiesContext 대신 쿼리를 사용하여 데이터베이스 초기화하도로 수정
    - @DirtiestContext 는 각 테스트 메서드를 수행하기 전에 ApplicationContext 를 새로 할당하므로 오래걸림
    - @DirtiestContext 대신 쿼리문을 사용함으로써 24개의 통합 테스트 실행 시간을 24초에서 3초로 단축

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -1,21 +1,33 @@
 package subway.integration;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class IntegrationTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    JdbcTemplate template;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+    }
+
+    @AfterEach
+    void tearDown() {
+        // 테스트 데이터베이스 초기화
+        template.update("delete from line;");
+        template.update("delete from station;");
+        template.update("delete from section;");
     }
 }
