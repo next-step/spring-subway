c23c01e7910328d29b805533f86635f112c1cf30
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-07-30 15:21:23 +0900

    Chore : deprecated 변수 패키지 이전

--- src/test/java/subway/integration/IntegrationTest.java
+++ src/test/java/subway/integration/IntegrationTest.java
@@ -3,7 +3,7 @@ package subway.integration;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
 @Sql({"classpath:schema.sql"})
