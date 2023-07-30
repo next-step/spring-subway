c89ac9e4bd6abc323afdb12225601bce4ed644ef
dahyen0o <seeunchoi99@gmail.com>
2023-07-30 16:06:22 +0900

    feat: PathManagerTest의 '출발역과 도착역으로 최단 경로를 찾는 데 성공한다' 기능 구현

--- src/main/java/subway/domain/PathManager.java
+++ src/main/java/subway/domain/PathManager.java
@@ -1,17 +1,36 @@
 package subway.domain;
 
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 import java.util.List;
 
 public final class PathManager {
 
-    private PathManager() {
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> subwayMap;
+
+    public PathManager(final WeightedMultigraph<Station, DefaultWeightedEdge> subwayMap) {
+        this.subwayMap = subwayMap;
     }
 
     public static PathManager create(final List<Station> stations, final List<Section> sections) {
-        return new PathManager();
+        final WeightedMultigraph<Station, DefaultWeightedEdge> subwayMap = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+        stations.forEach(subwayMap::addVertex);
+        sections.forEach(section -> subwayMap.setEdgeWeight(
+                subwayMap.addEdge(section.getUpStation(), section.getDownStation()),
+                section.getDistance()
+        ));
+        return new PathManager(subwayMap);
     }
 
     public List<Station> findStationsOfShortestPath(final Station source, final Station target) {
-        throw new UnsupportedOperationException();
+        final GraphPath<Station, DefaultWeightedEdge> shortestPath = dijkstraShortestPath().getPath(source, target);
+        return shortestPath.getVertexList();
+    }
+
+    private DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath() {
+        return new DijkstraShortestPath<>(subwayMap);
     }
 }
