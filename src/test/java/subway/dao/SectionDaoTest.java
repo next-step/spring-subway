33269a7fdaf4db9d49dd6be4b695c40230f11fb6
jjungk <jsk33a@gmail.com>
2023-08-02 00:13:15 +0900

    refactor: SectionDaoTest method 별 given 코드 추가

--- src/test/java/subway/dao/SectionDaoTest.java
+++ src/test/java/subway/dao/SectionDaoTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,53 +22,69 @@ import subway.domain.Station;
 @DisplayName("SectionDaoTest")
 class SectionDaoTest {
 
-    private Section expectedSection;
-
     @Autowired
     private SectionDao sectionDao;
 
-    @BeforeEach
-    public void setup() {
-        expectedSection = new Section(
+    @Test
+    @DisplayName("전체 조회 테스트")
+    void findAll() {
+        // given
+        Section expectedSection = new Section(
             1L,
             new Station(1L, "부천시청역"),
             new Station(2L, "신중동역"),
             new Line(1L, "7호선", "주황"),
             10
         );
-    }
 
-    @Test
-    @DisplayName("전체 조회 테스트")
-    void findAll() {
+        // when
         List<Section> result = sectionDao.findAll();
-        assertThat(result).contains(expectedSection);
+
+        // then
+        assertThat(result).containsAll(List.of(expectedSection));
     }
 
     @Test
     @DisplayName("단일 조회 테스트")
     void findById() {
+        // given
+        Section expectedSection = new Section(
+            1L,
+            new Station(1L, "부천시청역"),
+            new Station(2L, "신중동역"),
+            new Line(1L, "7호선", "주황"),
+            10
+        );
+
+        // when
         Section result = sectionDao.findById(1L).get();
+
+        // then
         assertThat(result).isEqualTo(expectedSection);
     }
 
     @Test
     @DisplayName("데이터 입력 테스트")
     void insert() {
+        // given
         Section addSection = new Section(
             new Station(3L, "춘의역"),
             new Station(4L, "부천종합운동장역"),
             new Line(1L, "7호선", "주황"),
             20
         );
+
+        // when
         Section result = sectionDao.insert(addSection);
+
+        // then
         assertThat(result.getId()).isNotNull();
     }
 
     @Test
     @DisplayName("데이터 업데이트 테스트")
     void update() {
-
+        // given
         Section updateSection = new Section(
             1L,
             new Station(1L, "부천시청역"),
@@ -78,7 +93,10 @@ class SectionDaoTest {
             15
         );
 
+        // when
         sectionDao.update(updateSection);
+
+        // then
         assertThat(sectionDao.findById(1L)
             .orElseThrow(() -> new IllegalStateException("값이 없습니다.")))
             .isEqualTo(updateSection);
@@ -87,7 +105,13 @@ class SectionDaoTest {
     @Test
     @DisplayName("Section 데이터 삭제 테스트")
     void delete() {
-        sectionDao.deleteById(1L);
+        // given
+        Long deleteSectionId = 1L;
+
+        // when
+        sectionDao.deleteById(deleteSectionId);
+
+        // then
         Assertions.assertThatThrownBy(
                 () -> sectionDao.findById(1L)
                     .orElseThrow(() -> new IllegalStateException("값이 없습니다."))
@@ -98,19 +122,39 @@ class SectionDaoTest {
     @Test
     @DisplayName("lineId에 해당하는 Section 리스트 반환 테스트")
     void findAllByLineId() {
-        Sections result1 = sectionDao.findAllByLineId(1L);
-        Sections result2 = sectionDao.findAllByLineId(0L);
+        // given
+        Section section = new Section(
+            1L,
+            new Station(1L, "부천시청역"),
+            new Station(2L, "신중동역"),
+            new Line(1L, "7호선", "주황"),
+            10
+        );
+        List<Section> sections = List.of(section);
+
+        // when
+        Sections result1 = sectionDao.findAllByLineId(0L);
+        Sections result2 = sectionDao.findAllByLineId(1L);
+
+        // then
         assertAll(
-            () -> assertThat(result1).isEqualTo(new Sections(List.of(expectedSection))),
-            () -> assertThat(result2).isEqualTo(new Sections(List.of()))
+            () -> assertThat(result1).isEqualTo(new Sections(List.of())),
+            () -> assertThat(result2).isEqualTo(new Sections(sections))
         );
     }
 
     @Test
     @DisplayName("역 구간 삭제 테스트")
     void deleteByDownStationId() {
-        sectionDao.deleteByDownStationIdAndLineId(2L, 1L);
-        assertThat(sectionDao.findById(1L).isEmpty())
-            .isTrue();
+        // given
+        Long deletedSectionId = 1L;
+        Long deleteLineId = 1L;
+        Long deleteDownStationId = 2L;
+
+        // when
+        sectionDao.deleteByDownStationIdAndLineId(deleteDownStationId, deleteLineId);
+
+        // then
+        assertThat(sectionDao.findById(deletedSectionId).isEmpty()).isTrue();
     }
 }
